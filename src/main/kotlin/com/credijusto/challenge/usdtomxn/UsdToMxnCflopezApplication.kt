23bcf2d1daa8fb510aa1bca325836d8f5821dd00
Carlos F López <cgauss@gmail.com>
2021-06-21 18:21:36 -0500

    First commit, Spring Initializr

---
+++ src/main/kotlin/com/credijusto/challenge/usdtomxn/UsdToMxnCflopezApplication.kt
@@ -0,0 +1,11 @@
+package com.credijusto.challenge.usdtomxn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsdToMxnCflopezApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsdToMxnCflopezApplication>(*args)
+}
