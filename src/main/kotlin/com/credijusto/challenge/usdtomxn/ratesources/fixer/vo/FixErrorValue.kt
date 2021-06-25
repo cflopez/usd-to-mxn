b28dd5616d8851c1b6f342e37603aecda1fcb4f6
SkyAngel Desarrollo <desarrollo@skyangel.com.mx>
2021-06-25 17:40:46 -0500

    Second site fully implemented. Added Coroutine execution

---
+++ src/main/kotlin/com/credijusto/challenge/usdtomxn/ratesources/fixer/vo/FixErrorValue.kt
@@ -0,0 +1,14 @@
+package com.credijusto.challenge.usdtomxn.ratesources.fixer.vo
+
+class ErrorFixValue (code: Int, type: String)
+{
+
+
+    {
+        "success":false,
+        "error":{
+        "code":105,
+        "type":"base_currency_access_restricted"
+    }
+    }
+}
